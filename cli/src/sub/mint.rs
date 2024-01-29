01eb30a099edf2a93af18ff98b35b1aaacf7d9f3
tulliusder <tulliusder@protonmail.com>
2024-01-29 23:10:52 +0800

    add mint vrc20 cmd

--- cli/src/sub/mint.rs
+++ cli/src/sub/mint.rs
@@ -1,5 +1,6 @@
-use anyhow::{Context as AnyhowContext, Result};
+use anyhow::{anyhow, bail, Context as AnyhowContext, Result};
 use clap::Subcommand;
+use vital_script_primitives::{resources::Name, traits::EnvContext};
 
 use crate::Cli;
 
@@ -7,10 +8,18 @@ use super::context::{build_context, Context};
 
 #[derive(Debug, Subcommand)]
 pub enum MintSubCommands {
-    /// Query vitalicals status.
+    /// Mint Name resource.
     Name {
         /// The name to mint
         name: String,
+
+        /// The sat amount in BTC to send.
+        amount: u64,
+    },
+    /// Mint VRC20 resource by it 's name.
+    VRC20 {
+        /// The vrc20 's name to mint.
+        vrc20_name: String,
 
         /// The sat amount in BTC to send.
         amount: u64,
@@ -25,6 +34,11 @@ impl MintSubCommands {
 
                 mint_name(&context, name.clone()).await?;
             }
+            Self::VRC20 { vrc20_name, amount } => {
+                let context = build_context(cli).await?.with_amount(*amount);
+
+                mint_vrc20(&context, vrc20_name.clone()).await?;
+            }
         }
 
         Ok(())
@@ -37,6 +51,40 @@ async fn mint_name(context: &Context, name: String) -> Result<()> {
     // build script.
     let output_index = 0_u32;
     let scripts_bytes = templates::mint_name(output_index, name).context("build scripts failed")?;
+
+    // build tx then send
+    crate::send_p2tr(context, scripts_bytes).await.context("send_p2tr failed")?;
+
+    Ok(())
+}
+
+async fn mint_vrc20(context: &Context, vrc20_name: String) -> Result<()> {
+    use vital_script_builder::templates;
+
+    let name = Name::try_from(vrc20_name.as_str())
+        .with_context(|| format!("the vrc20 name {} format invalid", vrc20_name))?;
+
+    let vrc20_metadata = context
+        .query_env_context
+        .get_vrc20_metadata(name)
+        .context("get vrc20 metadata")?
+        .ok_or_else(|| anyhow!("not found vrc20 metadata by {}", name))?;
+
+    if vrc20_metadata.mint_count >= vrc20_metadata.meta.mint.max_mints {
+        bail!(
+            "the vrc20 mint count is {}, and it had reached it 's max mint count {}, so the mint will failed",
+            vrc20_metadata.mint_count,  vrc20_metadata.meta.mint.max_mints
+        );
+    }
+
+    if vrc20_metadata.meta.mint.mint_height > 0 {
+        // TODO: impl mint height check.
+    }
+
+    // build script.
+    let output_index = 0_u32;
+    let scripts_bytes =
+        templates::mint_vrc20(output_index, name).context("build scripts failed")?;
 
     // build tx then send
     crate::send_p2tr(context, scripts_bytes).await.context("send_p2tr failed")?;
--- vital-script/builder/src/templates/mod.rs
+++ vital-script/builder/src/templates/mod.rs
@@ -4,6 +4,8 @@ mod utils;
 
 mod deploy_vrc20;
 mod mint_name;
+mod mint_vrc20;
 
 pub use deploy_vrc20::*;
 pub use mint_name::*;
+pub use mint_vrc20::*;
